6a9d9b079cab488d117e50d59806dc50d3220762
atulGupta2229 <31149725+atulGupta2229@users.noreply.github.com>
2017-08-19 22:55:26 +0530

    js uploaded

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pardor 2.0</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-sm-12" id="nav">
+			<div class="col-sm-2">
+				
+			</div>
+			<div class="col-sm-8" style="padding-top: 30px;">
+			<div class="row">
+				<div class="col-sm-12">
+				<div class="col-sm-4">
+					<span class="h2" style="text-transform: uppercase;">Para<strong>dor</strong></span>
+				</div>
+				<div class="col-sm-8">
+					<ul>
+						<li>Home</li>
+						<li>Gallery</li>
+						<li>Contact</li>
+					</ul>
+				</div>
+			</div>
+			</div>				
+			</div>
+			<div class="col-sm-2">
+				
+			</div>			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12" id="banner" style="padding-top: 200px;">
+			<center>
+				<input type="text" name="search" class="input-group" id="search"><input type="submit" value="search" id="sbtn"><br>
+				<span style="font-size: 3em; color: white; background: rgba(0,0,0,0.7);padding: 10px;">Find Your Parador</span>
+			</center>
+		</div>
+	</div>
+</div>
+</body>
+</html>
