cb6b54b41b04d178448d31180430fcf08787dc6f
atulGupta2229 <31149725+atulGupta2229@users.noreply.github.com>
2017-08-20 16:46:05 +0530

    Work Sunday!!

--- index.php
+++ index.php
@@ -8,16 +8,15 @@
 	<script type="text/javascript">
 		$(document).ready(function(){
 			$(window).scroll(function(){
-				if(this.pageYOffset>50){
+				if(this.pageYOffset>=160){
 					$("#nav").attr("style","padding-top:0px");
-					$("li").attr("style","padding: 26px 26px 26px 26px;");
-					$("#nav").css({background:"rgba(96,125,139,1)",height:"80px"});
-					$("#hd").attr("style","padding-left: 60px;padding-top: 20px;");
+					$("li").attr("style","padding: 20px 33px 15px 33px;");
+					$("#nav").css({background:"rgba(96,125,139,1)",height:"60px"});
+					$("#hd").attr("style","padding-left: 100px;padding-top: 15px;font-size: 20px;");
 				}
-				if(this.pageYOffset<50){
-					$("#nav").css({background:"rgba(96,125,139,0.7)",height:"100px"});
-					$("li").attr("style","padding: 26px 26px 36px 26px;");
-					$("#nav").attr("style","padding-top:10px");
+				if(this.pageYOffset<160){
+					$("#nav").css({background:"rgba(96,125,139,0.7)",height:"80px"});
+					$("li").attr("style","padding: 33px 33px 23px 33px;");
 					$("#hd").attr("style","padding-left: 200px;padding-top: 20px;");
 				}
 			});
@@ -26,43 +25,88 @@
 </head>
 <body>
 <div class="container-fluid">
-	<div class="row">
-		<div class="col-sm-12" id="nav">
-			<div class="col-sm-10">
-			<div class="row">
-				<div class="col-sm-12">
-				<div class="col-sm-4"  style="padding-top: 20px; padding-left: 200px;" id="hd">
-					<span class="h2" style="text-transform: uppercase;">Para<strong>dor</strong></span>
-				</div>
-				<div class="col-sm-3">
-					
-				</div>
-				<div class="col-sm-5">
-					<ul>
-						<li>Home</li>
-						<li>Gallery</li>
-						<li>Contact</li>
-					</ul>
-				</div>
+		<div id="nav">
+			<div id="hd">
+				<span>para<strong>dor</strong></span>
 			</div>
-			</div>				
-			</div>
-			<div class="col-sm-2">
-				
+			<div id="menu">
+				<ul>
+					<li>Home</li>
+					<li>Top Hostels</li>
+					<li>Featured</li>
+				</ul>
 			</div>			
 		</div>
-	</div>
 	<div class="row">
 		<div class="col-sm-12" id="banner" style="padding-top: 200px;">
 			<center>
-				<input type="text" name="search" class="input-group" id="search"><span id="sbtn"><button type="submit" id="sbtn">Search</button></span><br><br>
+				<input type="text" name="search" class="input-group" id="search" placeholder="Search in your location..."><span id="sbtn"><button type="submit" id="sbtn">Search</button></span><br><br>
 				<span style="font-size: 3em; color: white; background: rgba(0,0,0,0.7);padding: 10px;">Find Your Parador</span>
 			</center>
 		</div>
 	</div>
 	<div class="row">
+		<div class="col-sm-12" id="hiw">
+			<center>
+				<span class="h1 text-center">How it WORKS?</span>
+			</center>
+			<div class="row" style="margin-top: 40px;">
+			<center>
+				<div class="col-sm-12">
+					<div class="col-sm-4">
+						<img src="assets/images/search.png" width="40%"><br><br>
+						<span class="h3">Explore</span>
+					</div>
+					<div class="col-sm-4">
+						<img src="assets/images/schedule.png" width="40%"><br><br>
+						<span class="h3">Parafy</span>
+					</div>
+					<div class="col-sm-4">
+						<img src="assets/images/move.png" width="40%"><br><br>
+						<span class="h3">Move</span>
+					</div>
+				</div>
+				</center>
+			</div>
+		</div>
+	</div>
+	<div class="row">
 		<div class="col-sm-12" id="featured">
-			
+			<div class="col-sm-4">
+			<center>
+				<div class="feat">
+					
+				</div>
+			</center>
+			</div>
+			<div class="col-sm-4">
+			<center>
+				<div class="feat">
+					
+				</div>
+			</center>
+			</div>
+			<div class="col-sm-4">
+			<center>
+				<div class="feat">
+					
+				</div>
+			</center>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12" id="hiw">
+			<center>
+				<span class="h1 text-center">The Team</span>
+			</center>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12" id="team">
+			<center>
+				<span class="h1 text-center">How it WORKS?</span>
+			</center>
 		</div>
 	</div>
 </div>
